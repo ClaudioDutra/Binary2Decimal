07f78370a516957a671f1939aa0439b5f0ac684c
Claudio <cbd@br.ibm.com>
2020-09-08 00:17:32 -0300

    Feature: Create logic to convert binary to decimal.
    Small adjusts in layout
    - Change background color
    - Add FontAwsome
    - Add Social media icons an link

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const binary =  {
     validate(value) {
         return value.toString() === '1' || value.toString() === '0'
+    },
+    convert(value) {
+        return parseInt(value, 2);
     }
 }
 
@@ -9,13 +12,16 @@ const input = {
         if(!binary.validate(value)) {
             input.val(input.val().slice(0, -1));
         }
+
+        if(!input.val()) {
+            input.val(0);
+        }
     }
 }
-
-
 
 $(document).ready(function(){
     $('fieldset input[name="binary"]').keyup(function(event){
       input.setValue($(this), event.key);
+      $('#result').text(binary.convert($(this).val()));
     });
 });
